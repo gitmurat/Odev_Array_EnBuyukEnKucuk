5852989059bab0dd40eb23397647b856e4e84eaa
murat kurt <murat.kurt@windowslive.com>
2022-06-23 00:06:20 +0300

    Add project files.

---
+++ Odev_Array_EnBuyukEnKucuk/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Odev_Array_EnBuyukEnKucuk
+{
+    internal class Program
+    {
+        static int[] EnleriBul(int[] sayilar)
+        {
+            Array.Sort(sayilar);
+            int[] enler = new int[2];
+            enler[0] = sayilar[0];
+            enler[1] = sayilar[sayilar.Length - 1];
+            
+            return enler;
+        }
+
+        static int[] DiziAl()
+        {
+            Console.Write("Kaç sayı girmek istediğinizi yazın: ");
+            int dizi_boyu = int.Parse(Console.ReadLine());
+            int[] dizi = new int[dizi_boyu];
+            Console.WriteLine("Sayılarınızı girin");
+            for(int i = 0; i < dizi_boyu; i++)
+            {
+                Console.Write(i+". Sayı: ");
+                dizi[i] = int.Parse(Console.ReadLine());
+            }
+            return dizi;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] giris = DiziAl();
+            int[] enler = EnleriBul(giris);
+
+            Console.WriteLine("En Küçük Sayı = " + enler[0]);
+            Console.WriteLine("En Büyük Sayı = " + enler[1]);
+        }
+    }
+}
---
+++ SortandWrite/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SortandWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 15, 9, 23, 2, 11 };
+            Array.Sort(sayilar);
+            int[] enler = new int[2];
+            int enKucuk = sayilar[0];
+            int enBuyuk = 0;
+            for (int i = 1; i < sayilar.Length; i++)
+            {
+                enBuyuk = sayilar[i];
+            }
+
+            enler[0] = enKucuk;
+            enler[1] = enBuyuk;
+
+            Console.WriteLine(enler[0] + " " + enler[1]);
+        }
+    }
+}
